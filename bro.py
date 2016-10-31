93e77d7f6af6b83ec94edba77da381f14c4f7f04
Shubhankar Dimri <dimrishubhi@gmail.com>
2016-11-01 02:19:14 +0530

    one click login update

--- bro.py
+++ bro.py
@@ -1,9 +1,15 @@
+#!/usr/bin/python
 from packages.brow import Brow
 import sys
 
 if __name__=='__main__':
 	obj=Brow()
-	if sys.argv[1]=="log":
+	
+	try:
+		if sys.argv[1]==None:
+			pass
+	except:
 		Brow.login(obj)
+	
 	if sys.argv[1]=='sea':
 		Brow.multiSearch(obj,''.join(sys.argv[2:]))
