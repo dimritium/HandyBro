577fd159459a8e50f3aa3f9d5b2bdc2e9a037d08
Shubhankar Dimri <dimrishubhi@gmail.com>
2017-03-29 21:51:37 +0530

    move hb to gitignore

--- packages/brow.py
+++ packages/brow.py
@@ -70,9 +70,9 @@ class Brow:
 		ua=UserAgent()
 		headers=ua.chrome
 		print headers
-		res=requests.get('http://www.google.com/search?q='+' '.join(sys.argv[1:]),headers=headers)
+		res=requests.get('http://www.google.com/search?q='+' '.join(sys.argv[1:]))
 		res.raise_for_status()
-		soup=bs4.BeautifulSoup(res.text,'lxml')
+		soup=bs4.BeautifulSoup(res.text,'html.parser')
 		linkElems=soup.select('.r a')
 		numOpen=min(5,len(linkElems))
 		for i in range(numOpen):
